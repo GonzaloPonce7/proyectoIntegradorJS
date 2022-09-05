3d3210c3fd2eba2a9dee07d1745044f6a5df4882
Gonzalo Ponce <gonzalo.p791@gmail.com>
2022-09-04 23:59:23 -0300

    entrega

--- js/script.js
+++ js/script.js
@@ -1,152 +1,90 @@
 // Clases
-class Cerveza {
-  constructor(estilo, nombre, precio, litros, img) {
-    this.estilo = estilo;
-    this.nombre = nombre;
-    this.precio = precio < 0 ? 0 : precio;
-    // this.precio = parseFloat(precio);
-    this.litros = litros < 0 ? 0 : litros;
-    this.img = img;
-  }
-
-  toString() {
-    return "Nombre: " + this.nombre + " x " + this.cantidad;
-  }
-}
-
 class Cliente {
   constructor(nombre, direccion, localidad, codigoPostal) {
     this.nombre = nombre;
     this.direccion = direccion;
     this.localidad = localidad;
     this.codigoPostal = codigoPostal;
-  }
-}
+  };
+};
+
+class Pedido {
+  constructor(cliente, carrito) {
+	this.cliente = cliente;
+	this.items = carrito.items;
+  };
+};
 
 class Item {
   constructor(cerveza, cantidad) {
     this.cerveza = cerveza;
     this.cantidad = cantidad;
   };
-
-  agregar(cantidad) {
-    this.cantidad += cantidad;
-  };
-
-  tieneDescuento() {
-    return this.cantidad > cantidadDescuento;
-  };
-
-  subtotal() {
-    if (this.tieneDescuento()) {
-      return this.cantidad * this.cerveza.precio * 0.9;
-    }
-    return this.cantidad * this.cerveza.precio;
-  };
 };
 
 class Carrito {
   constructor() {
-    this.recuperarDeStorage();
     this.items = [];
   };
 
   agregar(cerveza, cantidad) {
-    let item = this.items.find((item) => item.cerveza.nombre == cerveza.nombre);
-    item ? this.incrementar(cerveza) : this.items.push (new Item (cerveza,cantidad));
+    console.log('agregar => cerveza:',JSON.stringify(cerveza));
+    let item = this.items.find((item) => item.cerveza.id == cerveza.id);
+    item ? item.cantidad ++ : this.items.push (new Item (cerveza,cantidad));
+    console.log('agregar => item:',JSON.stringify(this.items));
   };
 
-  incrementar(cerveza) {
-    //buscar si la cerveza se encuentra dentro de la lista.
-    //Si no la encuentra, agregar.
-    //Si la encuentra cantidad++
-    const indice = this.items.findIndex((item) => item.cerveza.nombre === cerveza.nombre)
-    this.items[indice].cantidad++;
-  };
-
-  borrar(cerveza) {
-    //buscar si la cerveza se encuentra dentro de la lista.
-    //si la encuentra, borrar
-    //si no la encuentra, no hacer nada
+  borrar(item) {
     let listafiltrada = this.items.filter(
-      (el) => el.cerveza != cerveza
+      (el) => el != item
     );
     this.items = listafiltrada;
   };
-
+  
   vaciar() {
     this.items = [];
   };
-
-  decrementar(cerveza) {
-    //buscar si la cerveza se encuentra dentro de la lista.
-    //si la encuentro, restar cantidad
-    //si no la encuentro, no hace nada.
-    let cervezaBuscada = this.items.find(
-      (el) => el.nombre === cerveza.nombre
-    );
-    if (cervezaBuscada != undefined) {
-      cervezaBuscada.cantidad--;
-    };
+  
+  incrementar(item) {
+    item.cantidad++
+  };
+  
+  decrementar(item) {
+    item.cantidad > 0 && item.cantidad--
   };
 
-  //Corregir listar
-  listar() {
-    let texto = "";
-    this.items.forEach((e) => (texto += e.mostrar()));
-    texto += "Total: $" + this.total();
-  };
-
-  guardarEnStorage(storage = "session") {
-    const tipoStorage = storage === "local" ? localStorage : sessionStorage;
-    tipoStorage.setItem("itemsCarrito", JSON.stringify(this.items));
-  };
-
-  //Usar funcion
-  recuperarDeStorage(storage = "session") {
-    const tipoStorage = storage === "local" ? localStorage : sessionStorage;
-    let delStorage = tipoStorage.getItem("itemCarrito");
-    //Si hay un json en el storage, parsear para this.items []. Si no, this.items = [].
-    //(delStorage == undefined) ? this.items = [] : this.items = JSON.parse(tipoStorage.getItem('itemsCarrito'));
-    this.items =
-      delStorage == undefined ? [] : JSON.parse(tipoStorage.getItem("itemsCarrito"));
-  };
-
-  totalIva() {
-    return this.precio * this.cantidad * 1.21;
+  totalIva(item) {
+    return item.cerveza.precio * item.cantidad * 1.21;
   };
 
   total() {
     //Sumar totalIva de cada cerveza y return
     let total = 0;
-    this.items.precio.forEach((e) => (total += e.totalIva()));
+    this.items.forEach((e) => (total += this.totalIva(e)));
     return total;
   };
 };
 
 
 //Funciones Dom
-//Revisar por que no se muestra la imagen en el comentario html.
-function mostrarCervezasEnDOM(array) {
-  const divCarritoItems = document.getElementById("carritoDiv");
-  //tomo el div del html y le creo el html dinamico
+
+function mostrarCervezasEnDOM(data) {
+
   divCarritoItems.innerHTML = "";
-  array.forEach((element, i) => {
+  data.forEach((element, i) => {
     let html = `<div class="card" id="tarjeta${element.nombre}">
-              <h3 class="card-header">Nombre: ${element.nombre}</h3>
-              <!--<img src="../img/Dragon.png" alt=${element.nombre} class="card-img-bottom">-->
-              <img src=${element.img} alt=${element.nombre} class="card-img-bottom"> 
-              <div class="card-body">
-                  <p class="card-text">Estilo: ${element.estilo}</p>
-                  <p class="card-text">Precio: ${element.precio}</p>
-                  <p class="card-text">Litros: ${element.litros}</p>
-              </div>
-              <button type="button" data-id="${i}" class="agregarCarrito">Agregar al carrito</button>
-              </div>`;
+    <h3 class="card-header">Nombre: ${element.nombre}</h3>
+    <img src=${element.img} alt=${element.nombre} class="card-img-bottom"> 
+    <div class="card-body">
+    <p class="card-text">Estilo: ${element.estilo}</p>
+    <p class="card-text">Precio: ${element.precio}</p>
+    <p class="card-text">Litros: ${element.litros}</p>
+    </div>
+    <button class="agregarCarrito" id="agregarCarrito" type="button" data-id="${element.id}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Agregar</button>
+    </div>`;
     divCarritoItems.innerHTML += html;
-    console.log("mostrarCervezas");
   });
-}
+};
 
 function mostrarCarrito() {
   
@@ -157,149 +95,142 @@ function mostrarCarrito() {
     <h3>${item.cerveza.nombre}</h3>
     <p>${item.cerveza.estilo}</p>
     <label for="cantidad_${i}">Cantidad</label>
-    <input id="cantidad_${i}" type="number" value="${item.cantidad}" min="0"> 
-    <!--<p>${item.cantidad}</p>-->
-    <button data-id="${i}" class="deleteBtn">Borrar</button>
+    <button data-id="${item.cerveza.id}" class="decrementarBtn">-</button>
+    <p id="cantidad_${i}" type="number"  min="0">${item.cantidad}</p>
+    <button data-id="${item.cerveza.id}" class="incrementarBtn">+</button> 
+    <button data-id="${item.cerveza.id}" class="deleteBtn">Borrar</button>
     </div>`;
   });
+
+  totalDiv.innerHTML = `
+  <p>Total + iva = ${carrito.total()}</p>`;
+
   for (const btn of deleteBtn) {
     btn.addEventListener("click", (e) => {
-      const indice = e.target.getAttribute('data-id');
-      const cervezaElegida = carrito.items[indice].cerveza;
-      carrito.borrar(cervezaElegida);
+      const id = e.target.getAttribute('data-id');
+      const itemElegido = carrito.items.find(e => e.cerveza.id == id);
+      carrito.borrar(itemElegido);
       mostrarCarrito();
+      guardarCarrito();
     });
   }
+
+  for (const btn of decrementarBtns) {
+    btn.addEventListener("click", (e) => {
+      const id = e.target.getAttribute('data-id');
+      const itemElegido = carrito.items.find(e => e.cerveza.id == id);
+      carrito.decrementar(itemElegido);
+      mostrarCarrito();
+    });
+  };
+
+  for (const btn of incrementarBtns) {
+    btn.addEventListener("click", (e) => {
+      const id = e.target.getAttribute('data-id');
+      const itemElegido = carrito.items.find(e => e.cerveza.id == id);
+      carrito.incrementar(itemElegido);
+      mostrarCarrito();
+    });
+  };
 };
 
 // Funciones
-
 function guardarCarrito() {
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-
-  fetch ('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify(carrito),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  sessionStorage.setItem("carrito", JSON.stringify(carrito));
 };
 
 function recuperarCarrito() {
-  return Object.assign(new Carrito(), JSON.parse(localStorage.getItem("carrito")));
+  return Object.assign(new Carrito(), JSON.parse(sessionStorage.getItem("carrito")));
 };
 
-function envio() {
-  
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    cliente.nombre = e.target.nombre.value;
-    cliente.direccion = e.target.direccion.value;
-    cliente.localidad = e.target.localidad.value;
-    cliente.codigoPostal = e.target.codigoPostal.value;
-    console.log(cliente);
-    mostrarListaCliente(cliente);
-  });
-  
-  const mostrarListaCliente = (cliente) => {
-    let contenedor = document.getElementById("listaCliente");
-    contenedor.innerHTML = `
-    <span>Datos para el envio:<span>
-    <h4> ${cliente.nombre}<h4>
-      <h4> ${cliente.direccion}<h4>
-      <h4> ${cliente.localidad}<h4>
-      <h4> ${cliente.codigoPostal}<h4>`;
-    };
-    
-  };
-  
-  // Main
-  
-  //Catalogo
-  const cervezas = [
-    new Cerveza("roja", "Drago", 100, 7, 1, "https://drive.google.com/file/d/1BTsxURRFrh9zMr4-JlOJ4Ov59JzELn0_/view?usp=sharing"),
-    new Cerveza("negra", "Larry guaits", 100, 7, 1, "https://drive.google.com/file/d/1jBMgHcF4wHkqnvNBCkG9y_9mXfT-nffQ/view?usp=sharing"),
-    new Cerveza("negra", "Apollo", 100, 7, 1, "https://drive.google.com/file/d/1DWp2POd9a-Z-nyCNZeVbllYi5YmnVYFd/view?usp=sharing"),
-    new Cerveza("rubia", "Ahora con trigo", 100, 7, 1, "https://drive.google.com/file/d/1mayTiyZX36uuIVHdgO0xleNuM828Fk2-/view?usp=sharing"),
-  ];
-  
-  //Variables globales
-  //const carrito = recuperarCarrito() == undefined ? recuperarCarrito() : new Carrito();
-  const carrito = recuperarCarrito() || new Carrito();
-  const cliente = new Cliente();
-  
-  //elementos del DOM
-  const main = document.getElementById("main");
-  const listaSeleccion = document.getElementById('listaSeleccion');
-  const deleteBtn = document.getElementsByClassName('deleteBtn');
-  const agregarbtn = document.getElementsByClassName('agregarCarrito');
-  const form = document.getElementById("formCliente");
-  const btnEnvio = document.getElementById("boton");
-  const btnComprar = document.getElementById("btnComprar");
-  
-  //Ejecucion funciones del DOM
-  const mostrar = mostrarCervezasEnDOM(cervezas);
-  const mCarrito = mostrarCarrito();
-  //const tarea = envio();
-  
-  
-  // Eventos
-  
-  for (const btn of agregarbtn) {
-    btn.addEventListener("click", (e) => {
-    const indice = e.target.getAttribute('data-id');
-    const cervezaElegida = cervezas[indice];
-    console.log(cervezaElegida)
-    // Ejecuto la función para agregar
-    carrito.agregar(cervezaElegida, 1);
-    mostrarCarrito(); // Actualiza la pantalla
-    guardarCarrito();
-  });
+const fetchCatalogo = async () => {
+  const resp = await fetch('/catalogo.json')
+  const dataCervezas = await resp.json()
+  mostrarCervezasEnDOM(dataCervezas);
+  cervezas = dataCervezas
+  agregarbtn = document.getElementsByClassName('agregarCarrito');
+  for (let i = 0; i < agregarbtn.length; i++) {
+    agregarbtn[i].addEventListener("click", (e) => {
+      const id = e.target.getAttribute('data-id');
+      const cervezaElegida = cervezas.find(e => e.id == id);
+      carrito.agregar(cervezaElegida, 1);
+      mostrarCarrito();
+      guardarCarrito();
+    });
+  };  
 };
 
+// Main
+
+//Variables globales
+const carrito = recuperarCarrito() || new Carrito();
+const cliente = new Cliente();
+let agregarbtn = undefined;
+let cervezas = [];
+fetchCatalogo()
+
+//elementos del DOM
+const main = document.getElementById("main");
+const listaSeleccion = document.getElementById('listaSeleccion');
+const deleteBtn = document.getElementsByClassName('deleteBtn');
+const incrementarBtns = document.getElementsByClassName('incrementarBtn');
+const decrementarBtns = document.getElementsByClassName('decrementarBtn');
+const form = document.getElementById("formCliente");
+const btnEnvio = document.getElementById("boton");
+const btnComprar = document.getElementById("btnComprar");
+const btnVaciar = document.getElementById("btnVaciar")
+const divCarritoItems = document.getElementById("carritoDiv");
+const totalDiv = document.getElementById("total");
+
+//Ejecucion funciones del DOM
+const mCarrito = mostrarCarrito();
+
+// Eventos
+
+btnVaciar.addEventListener("click", () => {
+  carrito.vaciar(carrito);
+  mostrarCarrito()
+  guardarCarrito()
+});
 
 btnComprar.addEventListener("click", async ()=>{
-  ''
+
   const { value: formValues } = await Swal.fire({
     title: 'Formulario de Envio',
-      html:
-      '<span>Nombre y Apellido: </span>' + 
-      '<input id="swal-input1" name="name" class="swal2-input">'+'<br>'+
-      '<span>Direccion: </span>'
-      +'<input id="swal-input2" name="direccion" class="swal2-input">' +
-      '<span>Localidad: </span>'
-      +'<input id="swal-input3" name="localidad" class="swal2-input">'+'<br>'+
-      '<span>Codigo Postal: </span>'+ '<br>' +
-      '<input id="swal-input4" name="codigo" class="swal2-input">',
-      focusConfirm: false,
-      preConfirm: () => {
-        return [
-          document.getElementById('swal-input1').value,
-          document.getElementById('swal-input2').value,
-          document.getElementById('swal-input3').value,
-          document.getElementById('swal-input4').value,
-        ]
-      }
-    })
-    
-    if (formValues) {
-      Swal.fire(`Gracias ${formValues.nombre}`)
-      //JSON.stringify(formValues)
-      fetch ('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify(formValues),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-
-      const nuevoCliente = new Cliente(formValues.nombre, formValues.direccion, formValues.localidad, formValues.codigoPostal);
-      console.log(nuevoCliente);
+    html:`
+    <label>Nombre y Apellido: </label>
+    <input id="swal-input1" name="name" class="swal2-input">
+    <label>Direccion: </label> 
+    <input id="swal-input2" name="direccion" class="swal2-input">
+    <label>Localidad: </label> 
+    <input id="swal-input3" name="localidad" class="swal2-input">
+    <label>Codigo Postal: </label> 
+    <input id="swal-input4" name="codigo" class="swal2-input">`,
+    focusConfirm: false,
+    preConfirm: () => {
+      return {
+        'nombre':document.getElementById('swal-input1').value,
+        'direccion':document.getElementById('swal-input2').value,
+        'localidad':document.getElementById('swal-input3').value,
+        'codigoPostal':document.getElementById('swal-input4').value,
+      };
     }
-    
   });
+
+  if (formValues) {
+    Swal.fire(`Gracias ${formValues.nombre} por tu compra`);
+    const nuevoCliente = new Cliente(formValues.nombre, formValues.direccion, formValues.localidad, formValues.codigoPostal);
+    const nuevoPedido = new Pedido(nuevoCliente,carrito);
+    localStorage.setItem("pedido",JSON.stringify(nuevoPedido));
+    console.log(nuevoPedido);
+    fetch ('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(nuevoPedido),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  };
+});
